78fc2cfbb60851f02b9be3874a59cdf8ffbb931e
kiseolar-ruslan <kretsu.rus@gmail.com>
2024-05-27 16:15:56 +0300

    add: lesson 32 'archive pages pagination'

--- wp-content/themes/firsttheme/archive-car.php
+++ wp-content/themes/firsttheme/archive-car.php
@@ -9,26 +9,30 @@
 
 get_header();
 ?>
-	<div>
-		<p>Template for Custom Post Type Car</p>
-		<header class="page-header">
-			<?php
-			the_archive_title( '<h1 class="page-title">', '</h1>' );
-			the_archive_description( '<div class="archive-description">', '</div>' );
-			?>
-		</header>
+    <div>
+        <p>Template for Custom Post Type Car</p>
+        <header class="page-header">
+            <?php
+            the_archive_title('<h1 class="page-title">', '</h1>');
+            the_archive_description('<div class="archive-description">', '</div>');
+            ?>
+        </header>
 
-		<?php
-		if ( have_posts() === true ) {
-			while ( have_posts() === true ) {
-				the_post();
-				get_template_part( 'partials/content' );
-			}
-		} else {
-			get_template_part( 'partials/content-none', 'none' );
-		}
-		?>
-	</div>
+        <?php
+        if (have_posts() === true) {
+            while (have_posts() === true) {
+                the_post();
+                get_template_part('partials/content');
+            }
+            //Pagination
+            echo '<div class="pagination">';
+            echo paginate_links();
+            echo '</div>';
+        } else {
+            get_template_part('partials/content-none', 'none');
+        }
+        ?>
+    </div>
 
 <?php
 get_sidebar('cars');
--- wp-content/themes/firsttheme/index.php
+++ wp-content/themes/firsttheme/index.php
@@ -1,20 +1,36 @@
 <?php
+
 get_header();
 ?>
 
 <div>
     <?php
-        if (have_posts() === true) {
-            while (have_posts() === true) {
-                the_post();
-                get_template_part('partials/content');
-            }
-        } else {
-            get_template_part('partials/content-none', 'none');
+    if (have_posts() === true) {
+        while (have_posts() === true) {
+            the_post();
+            get_template_part('partials/content');
         }
+        //Pagination
+        echo '<div class="pagination">';
+        the_posts_pagination(
+                array(
+                        'prev_text' => esc_html__('Prev', 'firsttheme'),
+                        'next_text' => esc_html__('Next', 'firsttheme')
+                )
+        );
+//        posts_nav_link(
+//            ' <--> ',
+//            esc_html__('Prev', 'firsttheme'),
+//            esc_html__('Next', 'firsttheme')
+//        );
+        echo '</div>';
+    } else {
+        get_template_part('partials/content-none', 'none');
+    }
     ?>
 </div>
 
-<?php //get_sidebar();
-get_footer();
+<?php
+//get_sidebar();
+//get_footer();
 ?>
